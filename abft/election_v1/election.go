9b6085cac23daed709c1ca7963b8744c6d06aa3f
Filip Drobnjaković <drobnjakovicfilip@gmail.com>
2024-12-11 17:59:27 +0100

    pre vote length opt

--- abft/election_v1/election.go
+++ abft/election_v1/election.go
@@ -2,12 +2,12 @@ package electionv1
 
 import (
 	"container/heap"
-	"errors"
 	"os"
 
 	"github.com/Fantom-foundation/lachesis-base/hash"
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
 	"github.com/Fantom-foundation/lachesis-base/inter/pos"
+	"github.com/viterin/vek"
 	"github.com/viterin/vek/vek32"
 )
 
@@ -32,15 +32,13 @@ type Election struct {
 	forklessCauses ForklessCauseFn
 	getFrameRoots  GetFrameRootsFn
 
-	vote     map[idx.Frame]map[hash.Event]map[idx.ValidatorID]bool
-	decided  map[idx.Frame]map[idx.ValidatorID]bool
-	eventMap map[idx.Frame]map[idx.ValidatorID]hash.Event
+	eventMap  map[idx.Frame]map[idx.ValidatorID]hash.Event
+	delivered map[idx.Frame]struct{}
 
-	votes        map[hash.Event][]float32
-	stake        []float32
-	valMap       map[idx.ValidatorID]idx.Validator
-	valNum       idx.Validator
-	maxSeenFrame idx.Frame
+	votes           map[hash.Event][]float32
+	valMap          map[idx.ValidatorID]idx.Validator
+	valNum          idx.Frame
+	emptyVoteVector []float32
 
 	deliveryBuffer heapBuffer
 	frameToDeliver idx.Frame
@@ -61,43 +59,27 @@ func New(
 }
 
 func (el *Election) Reset(validators *pos.Validators) {
-	el.vote = make(map[idx.Frame]map[hash.Event]map[idx.ValidatorID]bool)
 	el.eventMap = make(map[idx.Frame]map[idx.ValidatorID]hash.Event)
-	el.decided = make(map[idx.Frame]map[idx.ValidatorID]bool)
 	el.deliveryBuffer = make(heapBuffer, 0)
 	heap.Init(&el.deliveryBuffer)
 	el.frameToDeliver = 1
-	el.maxSeenFrame = 0
 	el.validators = validators
 	el.votes = make(map[hash.Event][]float32)
-	el.stake = make([]float32, 0, validators.Len())
-	el.valNum = validators.Len()
+	el.valNum = idx.Frame(validators.Len())
 	el.valMap = validators.Idxs()
+	el.delivered = make(map[idx.Frame]struct{})
+	el.emptyVoteVector = vek32.Repeat(-1., int(el.valNum))
 }
 
-func (el *Election) newFrameToDecide(frame idx.Frame) {
-	el.decided[frame] = make(map[idx.ValidatorID]bool)
-	el.eventMap[frame] = make(map[idx.ValidatorID]hash.Event)
+func (el *Election) newRoot(frame idx.Frame, validatorId idx.ValidatorID, root hash.Event) {
+	if _, ok := el.eventMap[frame]; !ok {
+		el.eventMap[frame] = make(map[idx.ValidatorID]hash.Event)
+	}
+	el.eventMap[frame][validatorId] = root
 }
 
 func (el *Election) decidedFrameCleanup(frame idx.Frame) {
-	delete(el.decided, frame)
 	delete(el.eventMap, frame)
-}
-
-func (el *Election) PutInYourVotes(voterMatr []float32, frame idx.Frame, observedRoots []EventDescriptor) {
-	// frame=2: voteMatr = [] -1 -1 -1 -1 -1]
-	// frame=3: voteMatr = [agg1, agg2, agg3, agg4, agg5] -1 -1 -1 -1 -1]
-	for idx := idx.Validator(0); idx < el.valNum; idx++ {
-		voterMatr = append(voterMatr, -1.)
-	}
-
-	// frame=2: voteMatr = [seen1, seen2, seen3, seen4, seen5]
-	// frame=3: voteMatr = [agg1, agg2, agg3, agg4, agg5, seen1, seen2, seen3, seen4, seen5]
-	// All should be in range [-1, 1]
-	for _, seenRoot := range observedRoots {
-		voterMatr[idx.Validator(frame-2)*el.valNum+el.valMap[seenRoot.ValidatorID]] = 1.
-	}
 }
 
 // ProcessRoot calculates Atropos votes only for the new root.
@@ -107,46 +89,46 @@ func (el *Election) ProcessRoot(
 	validatorId idx.ValidatorID,
 	voterRoot hash.Event,
 ) ([]*AtroposDecision, error) {
-	el.maxSeenFrame = max(el.maxSeenFrame, frame)
-	if _, ok := el.eventMap[frame]; !ok {
-		el.newFrameToDecide(frame)
-	}
-	el.eventMap[frame][validatorId] = voterRoot
-
+	vek.SetAcceleration(false)
+	el.newRoot(frame, validatorId, voterRoot)
 	if frame == 1 {
-		return []*AtroposDecision{}, nil
-	} else if frame == 2 {
-		el.votes[voterRoot] = make([]float32, 0, el.valNum)
-		el.PutInYourVotes(el.votes[voterRoot], frame, el.observedRoots(voterRoot, frame-1))
+		el.votes[voterRoot] = make([]float32, 0)
 		return []*AtroposDecision{}, nil
 	}
-	// valNum=5
-	// frame=2: voteMatr = [] 0 0 0 0 0]
-	// frame=3: voteMatr = [. . . . .] 0 0 0 0 0]
-	voterMatr := make([]float32, (frame-2)*idx.Frame(el.valNum), (frame-1)*idx.Frame(el.valNum))
-	decisionMatr := make([]float32, len(voterMatr))
+	voterMatr := make([]float32, (frame-2)*el.valNum, (frame-1)*el.valNum)
+	voteVec := vek32.Repeat(-1., int(el.valNum))
 
 	observedRoots := el.observedRoots(voterRoot, frame-1)
 	stakeAccul := float32(0)
 	for _, seenRoot := range observedRoots {
-		vek32.Add_Inplace(voterMatr, el.votes[seenRoot.EventID])
+		voteVec[el.valMap[seenRoot.ValidatorID]] = 1.
 		stakeAccul += float32(el.validators.GetWeightByIdx(el.validators.GetIdx(seenRoot.ValidatorID)))
+
+		vek32.Add_Inplace(voterMatr, el.votes[seenRoot.EventID])
 	}
-	copy(decisionMatr, voterMatr)
-	vek32.Div_Inplace(voterMatr, vek32.Abs(voterMatr))
+	if frame > 2 {
+		el.decideRoots(frame, voterMatr, stakeAccul)
+		vek32.Div_Inplace(voterMatr, vek32.Abs(voterMatr))
+	}
+	voterMatr = append(voterMatr, voteVec...)
+	vek32.MulNumber_Inplace(voterMatr, float32(el.validators.GetWeightByIdx(el.valMap[validatorId])))
+	el.votes[voterRoot] = voterMatr
+	return el.alignedAtropoi(), nil
+}
 
-	el.PutInYourVotes(voterMatr, frame, observedRoots)
-	vek32.MulNumber_Inplace(voterMatr, float32(el.validators.GetWeightByIdx(el.validators.GetIdx(validatorId))))
+func (el *Election) decideRoots(frame idx.Frame, aggregationMatr []float32, seenRootsStake float32) {
+	Q := (4.*float32(el.validators.TotalWeight()) - 3*seenRootsStake) / 4
+	yesDecisions := vek32.GtNumber(aggregationMatr, Q)
+	noDecisions := vek32.LtNumber(aggregationMatr, -Q)
 
-	Q := (4.*float32(el.validators.TotalWeight()) - 3*stakeAccul) / 4
-	yesDecisions := vek32.GtNumber(decisionMatr, Q)
-	noDecisions := vek32.LtNumber(decisionMatr, -Q)
-
-	for f := el.frameToDeliver; f <= el.maxSeenFrame-2; f++ {
+	for f := range el.eventMap {
+		if _, ok := el.delivered[f]; ok || f >= frame-1 {
+			continue
+		}
 		for _, v := range el.validators.SortedIDs() {
-			offset := (idx.Validator(f)-1)*el.valNum + el.validators.GetIdx(v)
+			offset := (f-1)*el.valNum + idx.Frame(el.validators.GetIdx(v))
 			if yesDecisions[offset] {
-				heap.Push(&el.deliveryBuffer, AtroposDecision{f, el.eventMap[f][v]})
+				heap.Push(&el.deliveryBuffer, &AtroposDecision{f, el.eventMap[f][v]})
 				el.decidedFrameCleanup(f)
 				break
 			}
@@ -155,69 +137,7 @@ func (el *Election) ProcessRoot(
 			}
 		}
 	}
-	return el.alignedAtropoi(), nil
-}
 
-func (el *Election) getVoteVector(frame idx.Frame, event hash.Event) map[idx.ValidatorID]bool {
-	if _, ok := el.vote[frame][event]; !ok {
-		el.vote[frame][event] = make(map[idx.ValidatorID]bool)
-	}
-	return el.vote[frame][event]
-}
-
-func (el *Election) yesVote(frame idx.Frame, root hash.Event) {
-	observedRoots := el.observedRoots(root, frame)
-	for _, candidateRoot := range observedRoots {
-		el.eventMap[frame][candidateRoot.ValidatorID] = candidateRoot.EventID
-		voteVector := el.getVoteVector(frame, root)
-		voteVector[candidateRoot.ValidatorID] = true
-	}
-}
-
-func (el *Election) aggregateVotes(
-	frameToDecide idx.Frame,
-	frame idx.Frame,
-	voterRoot hash.Event,
-) error {
-	observedRoots := el.observedRoots(voterRoot, frame-1)
-	for _, validator := range el.validators.IDs() {
-		if _, ok := el.decided[frameToDecide][validator]; ok {
-			continue
-		}
-		yesVotes := el.validators.NewCounter()
-		noVotes := el.validators.NewCounter()
-		for _, observedRoot := range observedRoots {
-			vote, ok := el.vote[frameToDecide][observedRoot.EventID][validator]
-			if ok && vote {
-				yesVotes.Count(observedRoot.ValidatorID)
-			} else {
-				noVotes.Count(observedRoot.ValidatorID)
-			}
-		}
-		if yesVotes.HasQuorum() || noVotes.HasQuorum() {
-			el.decided[frameToDecide][validator] = yesVotes.HasQuorum()
-		} else {
-			voteVector := el.getVoteVector(frameToDecide, voterRoot)
-			voteVector[validator] = yesVotes.Sum() >= noVotes.Sum()
-		}
-	}
-	return nil
-}
-
-func (el *Election) chooseAtropos(frame idx.Frame) (*AtroposDecision, error) {
-	for _, validatorId := range el.validators.SortedIDs() {
-		decision, ok := el.decided[frame][validatorId]
-		if !ok {
-			return nil, nil // no new decisions
-		}
-		if decision {
-			return &AtroposDecision{
-				frame,
-				el.eventMap[frame][validatorId],
-			}, nil
-		}
-	}
-	return nil, errors.New("all the roots are decided as 'no', which is possible only if more than 1/3W are Byzantine")
 }
 
 func (el *Election) observedRoots(root hash.Event, frame idx.Frame) []EventDescriptor {
@@ -245,8 +165,14 @@ func DBG(s string) {
 func (el *Election) alignedAtropoi() []*AtroposDecision {
 	deliveredAtropoi := make([]*AtroposDecision, 0)
 	for len(el.deliveryBuffer) > 0 && el.deliveryBuffer[0].Frame == el.frameToDeliver {
+		el.delivered[el.deliveryBuffer[0].Frame] = struct{}{}
 		deliveredAtropoi = append(deliveredAtropoi, heap.Pop(&el.deliveryBuffer).(*AtroposDecision))
 		el.frameToDeliver++
+	}
+	for f := el.frameToDeliver - idx.Frame(len(deliveredAtropoi)); f < el.frameToDeliver; f++ {
+		for v := range el.eventMap[f] {
+			delete(el.votes, el.eventMap[f][v])
+		}
 	}
 	return deliveredAtropoi
 }
